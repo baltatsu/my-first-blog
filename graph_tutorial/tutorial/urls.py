434e2b6ffe553c82d4c1c4e006c38ac5e697edcd
baltatsu <tatsuya.oie@eizo.com>
2019-01-10 11:28:41 +0900

    Added views to create/edit blog post inside the site.

--- graph_tutorial/tutorial/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from tutorial import views
-
-urlpatterns = [
-    path('tutorial/', include('tutorial.urls')),
-    path('admin/', admin.site.urls),
-]
--- graph_tutorial/tutorial/views.py
+++
@@ -1,5 +0,0 @@
-from django.http import HttpResponse, HttpResponseRedirect
-
-def home(request):
-  # Temporary!
-  return HttpResponse("Welcome to the tutorial.")
