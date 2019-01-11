aa0621deaa3ff17db024c6b572fff068f948919b
baltatsu <tatsuya.oie@eizo.com>
2019-01-11 16:23:43 +0900

    Added form to add comment, and login page.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'zebtcsi@b#ws-k9%l)v2p6a2cumzizq(1^lx+)dob@zy87bf1f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL ='/'
